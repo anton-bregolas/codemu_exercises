54beaaa3829e4cd897705c4d3457f59aaa8f7153
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2023-03-29 00:05:12 +0300

    JS Functions Practice 171 (3) - array from string func replaced

--- jsPmUFPrm-func-make-array-from-string.js
+++ jsPmUFPrm-func-make-array-from-string.js
@@ -1,28 +1,14 @@
-// №4
+// №3
 
-// Сделайте функцию, которая параметром будет принимать строку и переворачивать ее символы в обратном порядке.
+// Сделайте функцию, которая параметром будет принимать строку и возвращать массив ее символов.
 
-function reverseYourString(str) {
+function makeArrayFromString(str) {
 
-    // using in-built methods:
-    
-    // let revStr = String(str).split('').reverse().join('');
+    let strArray = String(str).split('');
 
-    // using for loop:
+    return strArray;
 
-    let revStr = '';
-
-    if (typeof str !== 'string') {
-        str = String(str);
-    }
-
-    for (let i = str.length - 1; i >= 0; i--) {
-        revStr += str[i];
-    }
-
-    return revStr;
 }
 
-let strToRev = "Hello Agent Cooper";
-console.log(reverseYourString(strToRev));
-console.log(reverseYourString(430));
+console.log(makeArrayFromString(12345));
+console.log(makeArrayFromString("asdfg"));
